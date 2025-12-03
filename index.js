2d42f4207e652e8b16043762aac2ddaa278400f9
kennethutnlinux <kenneth.utn.linux@gmail.com>
2025-12-03 03:17:53 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname)));
+
+// Servir index.html en la raíz
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
